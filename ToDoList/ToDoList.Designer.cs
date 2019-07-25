7e729a54783a52e0ef9208cdaee15ae2e63342aa
fujikirina <rina@fujikirina.work>
2019-07-25 22:51:12 +0900

    ToDoList

---
+++ ToDoList/ToDoList.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace todoList
+{
+    partial class ToDoList
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ToDoList));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.削除ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 6);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 21;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.dataGridView1.Size = new System.Drawing.Size(500, 550);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.AccessibleDescription = "day";
+            this.dateTimePicker1.AccessibleName = "day";
+            this.dateTimePicker1.Checked = false;
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(518, 144);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(4);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(253, 30);
+            this.dateTimePicker1.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(518, 216);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(253, 30);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(514, 117);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "期限";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(514, 189);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "やること";
+            // 
+            // button1
+            // 
+            this.button1.AccessibleDescription = "submit";
+            this.button1.Font = new System.Drawing.Font("游ゴシック", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(532, 306);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(230, 74);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "登録";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AccessibleDescription = "delete";
+            this.button2.Font = new System.Drawing.Font("游ゴシック", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(532, 415);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(230, 74);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "全削除";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.削除ToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(203, 28);
+            // 
+            // 削除ToolStripMenuItem
+            // 
+            this.削除ToolStripMenuItem.Font = new System.Drawing.Font("メイリオ", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.削除ToolStripMenuItem.Name = "削除ToolStripMenuItem";
+            this.削除ToolStripMenuItem.Size = new System.Drawing.Size(202, 24);
+            this.削除ToolStripMenuItem.Text = "選択中の行を削除(&D)";
+            this.削除ToolStripMenuItem.Click += new System.EventHandler(this.削除ToolStripMenuItem_Click);
+            // 
+            // button4
+            // 
+            this.button4.AccessibleDescription = "save";
+            this.button4.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button4.Font = new System.Drawing.Font("游ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button4.Location = new System.Drawing.Point(532, 48);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(230, 35);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "完了済タスクの表示";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.Button4_Click_1);
+            // 
+            // button5
+            // 
+            this.button5.AccessibleDescription = "save";
+            this.button5.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.button5.Font = new System.Drawing.Font("游ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button5.Location = new System.Drawing.Point(532, 48);
+            this.button5.Margin = new System.Windows.Forms.Padding(4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(230, 35);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "完了済タスクの非表示";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
+            // ToDoList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("メイリオ", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.MaximumSize = new System.Drawing.Size(800, 600);
+            this.MinimumSize = new System.Drawing.Size(800, 600);
+            this.Name = "ToDoList";
+            this.Text = "ToDoアプリ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 削除ToolStripMenuItem;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
---
+++ ToDoList/ToDoList.cs
@@ -0,0 +1,306 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Xml;
+using System.Drawing;
+
+namespace todoList
+{
+    public partial class ToDoList : Form
+    {
+        public ToDoList()
+        {
+            InitializeComponent();
+        }
+
+        //アプリ起動時動作
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //列の幅と高さの変更不可
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            //新しい行の追加を非表示
+            dataGridView1.AllowUserToAddRows = false;
+            //行ヘッダーを非表示
+            dataGridView1.RowHeadersVisible = false;
+            //行と列の幅を調整
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.None;
+            //選択は行単位
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            //列の追加
+            dataGridView1.Columns.Add(new DataGridViewCheckBoxColumn());
+            dataGridView1.ColumnCount = 3;
+            dataGridView1.Columns[0].HeaderText = "完了";
+            dataGridView1.Columns[1].HeaderText = "期限";
+            dataGridView1.Columns[2].HeaderText = "やること";
+            dataGridView1.Columns[0].Width = 60;
+            dataGridView1.Columns[1].Width = 105;
+
+            //コンテキストメニューの追加
+            dataGridView1.ContextMenuStrip = this.contextMenuStrip1;
+
+            //xmlファイルの読み込み
+            string dir = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+            string updir = dir.Substring(0, dir.LastIndexOf(@"\bin") +1);
+            XDocument xml = XDocument.Load(updir+"data.xml");
+
+            //データの取得
+            XElement table = xml.Element("data");
+            var rows = table.Elements("line");
+            
+            int i = 0;
+
+            //今日の日付
+            DateTime now = DateTime.Now;
+            string day = now.ToShortDateString();
+            button5.Visible = false;
+
+            foreach (XElement row in rows)
+            {
+                XElement submit = row.Element("submit");
+                var num1 = Boolean.Parse(submit.Value);
+                XElement date = row.Element("date");
+                XElement todo = row.Element("todo");
+
+                //チェックボックスに入力があったら表示しない
+                dataGridView1.Rows.Add(num1, date.Value, todo.Value);
+                if (num1)
+                {
+                    dataGridView1.Rows[i].Visible = false;
+                }
+
+                //日付が過ぎたものは背景をグレーにする
+                if (day.CompareTo(dataGridView1.Rows[i].Cells[1].Value)==0 || day.CompareTo(dataGridView1.Rows[i].Cells[1].Value) == 1) {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Gainsboro;
+                }
+
+                i++;
+            }
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            //テキストボックスに入力があった場合
+            if (textBox1.Text.Length>0)
+            {
+                string textValue = textBox1.Text;
+                string dateValue = dateTimePicker1.Text;
+                dataGridView1.Rows.Add("False", dateValue, textValue);
+            }
+
+            //データの保存
+            string dir = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+            string updir = dir.Substring(0, dir.LastIndexOf(@"\bin") + 1);
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlElement xmlData = xmlDocument.CreateElement("data");
+            xmlDocument.AppendChild(xmlData);
+            for (int n = 0; n < dataGridView1.Rows.Count; n++)
+            {
+                XmlElement lineElem = xmlDocument.CreateElement("line");
+                xmlData.AppendChild(lineElem);
+                XmlElement submitElem = xmlDocument.CreateElement("submit");
+                lineElem.AppendChild(submitElem);
+                XmlNode submitNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                submitNode.Value = (dataGridView1.Rows[n].Cells[0].Value).ToString();
+                submitElem.AppendChild(submitNode);
+                XmlElement dateElem = xmlDocument.CreateElement("date");
+                lineElem.AppendChild(dateElem);
+                XmlNode dateNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                dateNode.Value = (dataGridView1.Rows[n].Cells[1].Value).ToString();
+                dateElem.AppendChild(dateNode);
+                XmlElement todoElem = xmlDocument.CreateElement("todo");
+                lineElem.AppendChild(todoElem);
+                XmlNode todoNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                todoNode.Value = (dataGridView1.Rows[n].Cells[2].Value).ToString();
+                todoElem.AppendChild(todoNode);
+            }
+            xmlDocument.Save(updir + "data.xml");
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            //警告してOKが押されたら表示データを全削除する
+            DialogResult result = MessageBox.Show("現在表示しているリストが全部削除されます！" + Environment.NewLine + "本当に削除しますか？",
+                "警告！",
+                MessageBoxButtons.OKCancel,
+                MessageBoxIcon.Exclamation);
+
+            if (result == DialogResult.OK)
+            {
+                dataGridView1.Rows.Clear();
+
+                //データの保存
+                string dir = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+                string updir = dir.Substring(0, dir.LastIndexOf(@"\bin") + 1);
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlElement xmlData = xmlDocument.CreateElement("data");
+                xmlDocument.AppendChild(xmlData);
+                for (int n = 0; n < dataGridView1.Rows.Count; n++)
+                {
+                    XmlElement lineElem = xmlDocument.CreateElement("line");
+                    xmlData.AppendChild(lineElem);
+                    XmlElement submitElem = xmlDocument.CreateElement("submit");
+                    lineElem.AppendChild(submitElem);
+                    XmlNode submitNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                    submitNode.Value = (dataGridView1.Rows[n].Cells[0].Value).ToString();
+                    submitElem.AppendChild(submitNode);
+                    XmlElement dateElem = xmlDocument.CreateElement("date");
+                    lineElem.AppendChild(dateElem);
+                    XmlNode dateNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                    dateNode.Value = (dataGridView1.Rows[n].Cells[1].Value).ToString();
+                    dateElem.AppendChild(dateNode);
+                    XmlElement todoElem = xmlDocument.CreateElement("todo");
+                    lineElem.AppendChild(todoElem);
+                    XmlNode todoNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                    todoNode.Value = (dataGridView1.Rows[n].Cells[2].Value).ToString();
+                    todoElem.AppendChild(todoNode);
+                }
+                xmlDocument.Save(updir + "data.xml");
+            }
+        }
+
+            private void 削除ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // 選択されている行をすべて削除する
+            foreach (DataGridViewRow row in this.dataGridView1.SelectedRows)
+            {
+                this.dataGridView1.Rows.Remove(row);
+            }
+
+            //データの保存
+            string dir = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+            string updir = dir.Substring(0, dir.LastIndexOf(@"\bin") + 1);
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlElement xmlData = xmlDocument.CreateElement("data");
+            xmlDocument.AppendChild(xmlData);
+
+            for (int n = 0; n < dataGridView1.Rows.Count; n++)
+            {
+                XmlElement lineElem = xmlDocument.CreateElement("line");
+                xmlData.AppendChild(lineElem);
+                XmlElement submitElem = xmlDocument.CreateElement("submit");
+                lineElem.AppendChild(submitElem);
+                XmlNode submitNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                submitNode.Value = (dataGridView1.Rows[n].Cells[0].Value).ToString();
+                submitElem.AppendChild(submitNode);
+                XmlElement dateElem = xmlDocument.CreateElement("date");
+                lineElem.AppendChild(dateElem);
+                XmlNode dateNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                dateNode.Value = (dataGridView1.Rows[n].Cells[1].Value).ToString();
+                dateElem.AppendChild(dateNode);
+                XmlElement todoElem = xmlDocument.CreateElement("todo");
+                lineElem.AppendChild(todoElem);
+                XmlNode todoNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                todoNode.Value = (dataGridView1.Rows[n].Cells[2].Value).ToString();
+                todoElem.AppendChild(todoNode);
+            }
+            xmlDocument.Save(updir + "data.xml");
+        }
+
+        private void Button4_Click_1(object sender, EventArgs e)
+        {
+            //データの保存
+            string dir = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+            string updir = dir.Substring(0, dir.LastIndexOf(@"\bin") + 1);
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlElement xmlData = xmlDocument.CreateElement("data");
+            xmlDocument.AppendChild(xmlData);
+
+            for (int n = 0; n < dataGridView1.Rows.Count; n++)
+            {
+                XmlElement lineElem = xmlDocument.CreateElement("line");
+                xmlData.AppendChild(lineElem);
+                XmlElement submitElem = xmlDocument.CreateElement("submit");
+                lineElem.AppendChild(submitElem);
+                XmlNode submitNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                submitNode.Value = (dataGridView1.Rows[n].Cells[0].Value).ToString();
+                submitElem.AppendChild(submitNode);
+                XmlElement dateElem = xmlDocument.CreateElement("date");
+                lineElem.AppendChild(dateElem);
+                XmlNode dateNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                dateNode.Value = (dataGridView1.Rows[n].Cells[1].Value).ToString();
+                dateElem.AppendChild(dateNode);
+                XmlElement todoElem = xmlDocument.CreateElement("todo");
+                lineElem.AppendChild(todoElem);
+                XmlNode todoNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                todoNode.Value = (dataGridView1.Rows[n].Cells[2].Value).ToString();
+                todoElem.AppendChild(todoNode);
+            }
+            xmlDocument.Save(updir + "data.xml");
+
+            for (int i=0; i<dataGridView1.Rows.Count; i++)
+            {
+                dataGridView1.Rows[i].Visible = true;
+            }
+            button5.Visible = true;
+            button4.Visible = false;
+        }
+
+        private void Button5_Click(object sender, EventArgs e)
+        {
+            button4.Visible = true;
+            button5.Visible = false;
+
+            //データの保存
+            string dir = AppDomain.CurrentDomain.BaseDirectory.TrimEnd('\\');
+            string updir = dir.Substring(0, dir.LastIndexOf(@"\bin") + 1);
+            XmlDocument xmlDocument = new XmlDocument();
+            XmlElement xmlData = xmlDocument.CreateElement("data");
+            xmlDocument.AppendChild(xmlData);
+
+            for (int n = 0; n < dataGridView1.Rows.Count; n++)
+            {
+                XmlElement lineElem = xmlDocument.CreateElement("line");
+                xmlData.AppendChild(lineElem);
+                XmlElement submitElem = xmlDocument.CreateElement("submit");
+                lineElem.AppendChild(submitElem);
+                XmlNode submitNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                submitNode.Value = (dataGridView1.Rows[n].Cells[0].Value).ToString();
+                submitElem.AppendChild(submitNode);
+                XmlElement dateElem = xmlDocument.CreateElement("date");
+                lineElem.AppendChild(dateElem);
+                XmlNode dateNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                dateNode.Value = (dataGridView1.Rows[n].Cells[1].Value).ToString();
+                dateElem.AppendChild(dateNode);
+                XmlElement todoElem = xmlDocument.CreateElement("todo");
+                lineElem.AppendChild(todoElem);
+                XmlNode todoNode = xmlDocument.CreateNode(XmlNodeType.Text, "", "");
+                todoNode.Value = (dataGridView1.Rows[n].Cells[2].Value).ToString();
+                todoElem.AppendChild(todoNode);
+            }
+            xmlDocument.Save(updir + "data.xml");
+
+            //データの再読み込み
+            XDocument xml = XDocument.Load(updir + "data.xml");
+            XElement table = xml.Element("data");
+            var rows = table.Elements("line");
+            int i = 0;
+
+            dataGridView1.Rows.Clear();
+
+            DateTime now = DateTime.Now;
+            string day = now.ToShortDateString();
+
+            foreach (XElement row in rows)
+            {
+                XElement submit = row.Element("submit");
+                var num1 = Boolean.Parse(submit.Value);
+                XElement date = row.Element("date");
+                XElement todo = row.Element("todo");
+                dataGridView1.Rows.Add(num1, date.Value, todo.Value);
+                if (num1)
+                {
+                    dataGridView1.Rows[i].Visible = false;
+                }
+                if (day.CompareTo(dataGridView1.Rows[i].Cells[1].Value) == 0 || day.CompareTo(dataGridView1.Rows[i].Cells[1].Value) == 1)
+                {
+                    dataGridView1.Rows[i].DefaultCellStyle.BackColor = Color.Gainsboro;
+                }
+                i++;
+            }
+        }
+    }
+}
